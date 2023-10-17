b7aa7492e30a766d58c278d300a0a405419f3941
Christian Broms <cb@christianbroms.com>
2023-10-16 22:10:26 -0700

    increase max zoom level

--- basemap/elevation/tile_contours.sh
+++ basemap/elevation/tile_contours.sh
@@ -4,17 +4,17 @@ echo -e "\nTiling dataset...\n"
 
 # tile contours, creating layers elevation_1000 from 10-11, elevation_200 from 11-12, and elevation_40 from 12+
 # see https://github.com/nst-guide/terrain/blob/4671546d2a17b1ed4e71a2e1648e5ffc595c3217/README.md?plain=1#L452-L468
-tippecanoe -Z10 -z13 -P -y elevation -l contour_1000 \
+tippecanoe -Z10 -z20 -P -y elevation -l contour_1000 \
     -C 'jq "if .properties.elevation % 1000 == 0 then . else {} end"' \
     -o data/contour_1000.mbtiles \
     data/temp/*.geojson --force
 
-tippecanoe -Z11 -z13 -P -y elevation -l contour_200 \
+tippecanoe -Z11 -z20 -P -y elevation -l contour_200 \
     -C 'jq "if .properties.elevation % 200 == 0 then . else {} end"' \
     -o data/contour_200.mbtiles \
     data/temp/*.geojson --force
 
-tippecanoe -Z12 -z13 -P -y elevation -l contour_40 \
+tippecanoe -Z12 -z20 -P -y elevation -l contour_40 \
     -C 'jq "if .properties.elevation % 40 == 0 then . else {} end"' \
     -o data/contour_40.mbtiles \
     data/temp/*.geojson --force
