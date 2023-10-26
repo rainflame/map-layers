23e87ded00aa449d3ae97c913dc4510a4ab50b81
Christian Broms <cb@christianbroms.com>
2023-10-25 23:41:26 -0700

    max zoom to 18

--- basemap/elevation/tile_contours.sh
+++ basemap/elevation/tile_contours.sh
@@ -3,15 +3,15 @@
 echo -e "\nTiling dataset...\n"
 
 # tile contours, creating three separate files with 40ft, 200ft, and 1000ft contours at different zoom levels
-tippecanoe -Z10 -z20 -P -y elevation -l contour_1000 \
+tippecanoe -Z10 -z18 -P -y elevation -l contour_1000 \
     -o data/temp/contour_1000.mbtiles \
     data/temp/1000/*.geojsons --force --read-parallel
 
-tippecanoe -Z11 -z20 -P -y elevation -l contour_200 \
+tippecanoe -Z11 -z18 -P -y elevation -l contour_200 \
     -o data/temp/contour_200.mbtiles \
     data/temp/200/*.geojsons --force --read-parallel
 
-tippecanoe -Z12 -z20 -P -y elevation -l contour_40 \
+tippecanoe -Z12 -z18 -P -y elevation -l contour_40 \
     -o data/temp/contour_40.mbtiles \
     data/temp/40/*.geojsons --force --read-parallel
 
