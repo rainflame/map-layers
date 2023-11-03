266570d6a8fc1526f9ba518816c0ddb12fb1d749
Christian Broms <cb@christianbroms.com>
2023-11-02 18:57:20 -0700

    remove extra glacier labels input

--- basemap/tile_layers.sh
+++ basemap/tile_layers.sh
@@ -1,14 +1,21 @@
 layers=(
-    # "elevation/data/output/contours.mbtiles"
+    "elevation/data/output/contours.mbtiles"
     "glaciers/data/output/glaciers.mbtiles"
-    "glaciers/data/output/glacier_labels.mbtiles"
 )
 
 # filter out any files in the layers array that don't exist
 layers=($(for i in "${layers[@]}"; do if [ -f "$i" ]; then echo $i; fi; done))
 
-printf "\nCombining layers:\n\n"
+printf "\nFound layers to combine:\n\n"
 printf '%s\n' "${layers[@]}"
 printf "\n"
 
+echo -e "\nCombining layers...\n"
+
 tile-join -o data/output/basemap.mbtiles ${layers[@]} --force
+
+echo -e "\nConverting to pmtiles format...\n"
+
+pmtiles convert data/output/basemap.mbtiles data/output/basemap.pmtiles
+
+echo -e "\nDone, created: \ndata/output/basemap.mbtiles\ndata/output/basemap.pmtiles\n"
