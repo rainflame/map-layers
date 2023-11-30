475ae28cc5c8507f4496ba2633961cb7a71fa78e
Christian Broms <cb@christianbroms.com>
2023-11-29 23:54:58 -0500

    fix tiling overlap issue

--- basemap/elevation/create_contours.py
+++ basemap/elevation/create_contours.py
@@ -12,7 +12,9 @@ gdal.UseExceptions()
 
 
 @click.command()
-@click.option("--workers", default=multiprocessing.cpu_count(), help="Number of workers to use")
+@click.option(
+    "--workers", default=multiprocessing.cpu_count(), help="Number of workers to use"
+)
 def cli(workers):
     # get all .tif files in the data/sources/ directory
     files = glob.glob("data/sources/*.tif")
@@ -27,52 +29,51 @@ def cli(workers):
     except:
         pass
 
-    # use multiprocessing to create the contours, reporting progress with tqdm
+    # create a vrt file from the source files
+    gdal.BuildVRT("data/temp/elevation.vrt", files)
+
+    # scale the elevation values from meters to feet
+    translate_options = gdal.TranslateOptions(
+        format="VRT",
+        outputSRS="EPSG:4326",
+        outputType=gdal.GDT_Float32,
+        scaleParams=[[0.0, 0.3048, 0.0, 1.0]],
+    )
+
+    ds = gdal.Open("data/temp/elevation.vrt")
+    gdal.Translate("data/temp/elevation-feet.vrt", ds, options=translate_options)
+
+    intervals = [40, 200, 1000]
+
     with multiprocessing.Pool(processes=workers) as pool:
-        for _ in tqdm(pool.imap_unordered(create_contours, files), total=len(files)):
+        for _ in tqdm(
+            pool.imap_unordered(create_contours, intervals), total=len(intervals)
+        ):
             pass
-        pool.close()
-        pool.join()
 
 
-# scale the elevation values from meters to feet
-translate_options = gdal.TranslateOptions(
-    format="VRT",
-    outputSRS="EPSG:4326",
-    outputType=gdal.GDT_Float32,
-    scaleParams=[[0.0, 0.3048, 0.0, 1.0]],
-)
-
-
-def create_contours(path):
-    # get just the filename without the extension
-    file = path.split("/")[-1].split(".")[0]
-    ds = gdal.Open(path)
-    # create a virtual dataset from the source file converting it to feet
-    gdal.Translate("data/temp/{}.vrt".format(file), ds, options=translate_options)
-    vds = gdal.Open("data/temp/{}.vrt".format(file))
-
+def create_contours(interval):
+    vds = gdal.Open("data/temp/elevation-feet.vrt")
     sr = osr.SpatialReference()
     sr.ImportFromEPSG(4326)
 
-    # generate contours at 40, 200, and 1000 feet intervals
-    for i in [40, 200, 1000]:
+    # using geojsonseq to allow reading features in parallel with tippecanoe
+    out_dataset = ogr.GetDriverByName("GeoJSONSeq").CreateDataSource(
+        f"data/temp/{interval}/contour_{interval}.geojsons",
+    )
 
-        # using geojsonseq to allow reading features in parallel with tippecanoe
-        out_dataset = ogr.GetDriverByName("GeoJSONSeq").CreateDataSource(
-            "data/temp/{}/{}.geojsons".format(i, file),
+    contour_layer = out_dataset.CreateLayer("elevation", sr)
+    contour_layer.CreateField(ogr.FieldDefn("ID", ogr.OFTInteger))
+    contour_layer.CreateField(ogr.FieldDefn("elevation", ogr.OFTReal))
+
+    # create the contours
+    try:
+        gdal.ContourGenerate(
+            vds.GetRasterBand(1), interval, 0, [], 0, 0, contour_layer, 0, 1
         )
-
-        contour_layer = out_dataset.CreateLayer("elevation", sr)
-        contour_layer.CreateField(ogr.FieldDefn("ID", ogr.OFTInteger))
-        contour_layer.CreateField(ogr.FieldDefn("elevation", ogr.OFTReal))
-
-        # create the contours
-        try:
-            gdal.ContourGenerate(vds.GetRasterBand(1), i, 0, [], 0, 0, contour_layer, 0, 1)
-        except:
-            print("error generating contours for {}".format(file))
-            pass
+    except:
+        print(f"error generating contours for {interval}")
+        pass
 
 
 if __name__ == "__main__":
--- basemap/elevation/tile_contours.sh
+++ basemap/elevation/tile_contours.sh
@@ -1,22 +1,52 @@
 #!/bin/bash
 
-echo -e "\nTiling dataset...\n"
+# echo -e "\nFiltering out overlapping contours...\n"
 
-# tile contours, creating three separate files with 40ft, 200ft, and 1000ft contours at different zoom levels
-tippecanoe -Z10 -z16 -P -y elevation -l contour_1000 \
-    -o data/temp/contour_1000.pmtiles \
-    data/temp/1000/*.geojsons --force --read-parallel
+# ogr2ogr -f "GeoJSONSeq" \
+#             -sql "SELECT * FROM \"contour_200\" WHERE elevation % 1000 != 0" \
+#             "data/temp/200/contour_200_filtered.geojsons" \
+#             "data/temp/200/contour_200.geojsons" \
+#             -progress
+
+# ogr2ogr -f "GeoJSONSeq" \
+#             -sql "SELECT * FROM \"contour_40\" WHERE elevation % 1000 != 0" \
+#             "data/temp/40/contour_40_filtered.geojsons" \
+#             "data/temp/40/contour_40.geojsons" \
+#             -progress
+
+
+# echo -e "\nTiling dataset...\n"
+
+# # tile contours, creating three separate files with 40ft, 200ft, and 1000ft contours at different zoom levels
+# tippecanoe -Z10 -z16 -P -y elevation -l contour_1000 \
+#     data/temp/1000/contour_1000.geojsons --read-parallel \
+#     --drop-densest-as-needed \
+#     --simplification=5 \
+#     --hilbert \
+#     --visvalingam \
+#     -o data/temp/contour_1000.mbtiles \
+#     --force
 
 tippecanoe -Z11 -z16 -P -y elevation -l contour_200 \
-    -o data/temp/contour_200.pmtiles \
-    data/temp/200/*.geojsons --force --read-parallel
+    data/temp/200/contour_200_filtered.geojsons --read-parallel \
+    --drop-densest-as-needed \
+    --simplification=5 \
+    --hilbert \
+    --visvalingam \
+    -o data/temp/contour_200.mbtiles \
+    --force
 
 tippecanoe -Z12 -z16 -P -y elevation -l contour_40 \
-    -o data/temp/contour_40.pmtiles \
-    data/temp/40/*.geojsons --force --read-parallel
+    data/temp/40/contour_40_filtered.geojsons --read-parallel \
+    --drop-densest-as-needed \
+    --simplification=5 \
+    --hilbert \
+    --visvalingam \
+    -o data/temp/contour_40.mbtiles \
+    --force
 
 echo -e "\nMerging layers...\n"
 
-tile-join -o data/output/contours.pmtiles data/temp/contour_1000.pmtiles data/temp/contour_200.pmtiles data/temp/contour_40.pmtiles --force
+tile-join -o data/output/contours.mbtiles data/temp/contour_1000.mbtiles data/temp/contour_200.mbtiles data/temp/contour_40.mbtiles --force
 
-echo -e "\n\nDone, created:\ndata/output/contours.pmtiles\n"
+echo -e "\n\nDone, created:\ndata/output/contours.mbtiles\n"
