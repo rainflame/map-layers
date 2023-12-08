bc4997a8a69c2007f183b2576fa6ba2045ac090f
Christian Broms <cb@christianbroms.com>
2023-12-07 23:30:38 -0500

    fix filename

--- layers/snow/tile_snow.sh
+++ layers/snow/tile_snow.sh
@@ -2,10 +2,14 @@
 
 echo -e "\nConverting to GeoJSON...\n"
 
-ogr2ogr -f GeoJSONSeq data/temp/snow-contours.geojsons data/temp/snow-contours.shp
+ogr2ogr -f GeoJSONSeq data/temp/snow-contours.geojsons data/temp/snow-contours.gpkg
 
 echo -e "\nTiling dataset...\n"
 
-tippecanoe -Z1 -z16 -P -o data/output/snow.pmtiles --drop-densest-as-needed -l snow data/temp/snow-contours.geojsons --force
+tippecanoe -Z1 -z16 -P \
+         --drop-densest-as-needed \
+         -l snow data/temp/snow-contours.geojsons \
+         -o data/output/snow.pmtiles \
+          --force
 
 echo -e "\n\nDone, created: \ndata/output/snow.pmtiles\n"
