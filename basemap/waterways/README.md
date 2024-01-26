f4553ee8d962f8f71ff631b6446a086796e38d46
Christian Broms <cb@christianbroms.com>
2024-01-25 21:01:16 -0800

    use geopolymid for water labels

--- basemap/waterways/README.md
+++ basemap/waterways/README.md
@@ -34,6 +34,24 @@ python parse_waterways.py
 
 Now you should have water and waterways files at `data/temp/water.gpkg` and `data/temp/waterways.gpkg`.
 
+## Create label lines
+
+To place labels over water bodies such as lakes and reservoirs, we've built a tool called [geopolymid](https://github.com/rainflame/geopolymid). This tool will create smoothed lines that flow through the centers of each polygon and can be used to place a label.
+
+First, filter the water dataset so only water bodies of the correct type that have names are included:
+
+```
+ogr2ogr \
+    -f GPKG \
+    data/temp/water-no-rivers.gpkg \
+    data/temp/water.gpkg \
+    -where "(type != 'river' OR type IS NULL) AND name IS NOT NULL"
+```
+
+See [geopolymid](https://github.com/rainflame/geopolymid) for instructions on how to create label lines.
+
+Once complete, name the labels as `water-labels.gpkg` and place in `data/temp/water-labels.gpkg`.
+
 ## Tile
 
 Now we can create a tiled version of the water polygons and lines:
@@ -42,4 +60,4 @@ Now we can create a tiled version of the water polygons and lines:
 ./tile_waterways.sh
 ```
 
-Now you should have `data/output/water.pmtiles` and `data/output/waterways.pmtiles`.
+Now you should have `data/output/water.pmtiles`, which combines the water polygons, water labels, and waterway lines.
--- basemap/waterways/tile_waterways.sh
+++ basemap/waterways/tile_waterways.sh
@@ -1,29 +1,48 @@
 #!/bin/bash
 
-echo -e "\nConverting to GeoJSON...\n"
+echo "Converting to GeoJSON..."
 
 ogr2ogr -f GeoJSONSeq data/temp/water.geojsons data/temp/water.gpkg
 ogr2ogr -f GeoJSONSeq data/temp/waterways.geojsons data/temp/waterways.gpkg
+ogr2ogr -f GeoJSONSeq data/temp/water-labels.geojsons data/temp/water-labels.gpkg
 
-echo -e "\nTiling dataset...\n"
+echo "Tiling dataset..."
 
-tippecanoe -Z1 -z16 -P -o data/output/water.pmtiles \
+tippecanoe -Z11 -z16 -P \
+           -o data/temp/water-labels.pmtiles \
+           --drop-densest-as-needed \
+           -l water-labels \
+           data/temp/water-labels.geojsons \
+           --force
+
+tippecanoe -Z1 -z16 -P \
+            -o data/temp/water.pmtiles \
            --drop-densest-as-needed \
            --simplification=5 \
            --hilbert \
            --maximum-tile-bytes=350000 \
            --visvalingam \
-           -l water data/temp/water.geojsons \
+           -l water \
+           data/temp/water.geojsons \
            --force
 
-tippecanoe -Z1 -z16 -P -o data/output/waterways.pmtiles \
+tippecanoe -Z1 -z16 -P \
+            -o data/temp/waterways.pmtiles \
            --drop-densest-as-needed \
            --simplification=5 \
            --hilbert \
            --maximum-tile-bytes=350000 \
            --visvalingam \
-           -l waterways data/temp/waterways.geojsons \
+           -l waterways \
+           data/temp/waterways.geojsons \
            --force
-           
 
-echo -e "\n\nDone, created: \ndata/output/water.pmtiles\ndata/outupt/waterways.pmtiles\n"
+echo "Combining layers..."  
+
+tile-join -o data/output/water.pmtiles \
+          data/temp/waterways.pmtiles \
+          data/temp/water.pmtiles \
+          data/temp/water-labels.pmtiles
+
+
+echo -e "\n\nDone, created: \ndata/output/water.pmtiles"
