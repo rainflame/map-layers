57af9e9f55f1ef44757e004d8bbec0de524c53fa
Christian Broms <cb@christianbroms.com>
2024-04-21 19:10:59 -0700

    incorporate BLM boundaries

--- layers/places/README.md
+++ layers/places/README.md
@@ -14,22 +14,42 @@ mkdir -p data/sources/ && mkdir -p data/temp/ && mkdir -p data/output/
 
 ## Download data
 
-Use the provided osm download utility to download data from the [Overpass API](https://wiki.openstreetmap.org/wiki/Overpass_API). Pass in the bounding box as the first argument, ensuring that the boxes' corners are in the format lat, long. The second argument is the Overpass API query `(nwr[place];>;)`, which selects the required relations and ways, plus their nodes.
+### OSM data
+
+Use the provided osm download utility to download data from the [Overpass API](https://wiki.openstreetmap.org/wiki/Overpass_API). Pass in the bounding box as the first argument, ensuring that the boxes' corners are in the format lat, long. The second argument is the Overpass API query which selects the required relations and ways, plus their nodes.
 
 ```
 ./../../utils/download_osm.sh \
     "43.022586,-123.417224,45.278084,-118.980589" \
-    "(nwr[place];>;)"
+    "(nwr[place];>;nwr["boundary"];>;nwr[leisure=nature_reserve];>;nwr[leisure=park];>;nwr[leisure=dog_park];>;)"
 ```
 
-Now you should have the data at `data/sources/extract.osm.pbf`.
+Now you should have the OSM data at `data/sources/extract.osm.pbf`.
+
+### BLM boundaries
+
+BLM boundaries are not included in OSM data, so we'll download them separately. These can be found on the BLM's GIS site, for example here is the file for [Oregon](https://gbp-blm-egis.hub.arcgis.com/datasets/BLM-EGIS::blm-or-management-ownership-dissolve-polygon-hub/about_).
+
+Download as a geopackage and save in `/data/sources`.
+
+This file contains land ownership data information for a variety of juristictions besides the BLM. To reduce it so it only contains BLM boundaries, run this GDAL command:
+
+```
+ogr2ogr \
+    -f "GPKG" \
+    data/temp/BLM.gpkg \
+    data/sources/BLM_ownership.gpkg \
+    -where "PROPERTY_STATUS = 'BLM'"
+```
 
 ## Create geometries
 
-To convert the openstreetmap data into polygons and lines, run:
+To convert the openstreetmap data into polygons and lines, in addition to integrating the BLM boundaries, run:
 
 ```
 python parse_places.py
 ```
 
-Now you should have water and waterways files at `data/temp/water.gpkg` and `data/temp/waterways.gpkg`.
+Now you should have place point and polygons at `data/temp/place-points.gpkg` and `data/temp/place-polygons.gpkg`.
+
+## Tile
---
+++ layers/places/parse_places.py
@@ -0,0 +1,229 @@
+import osmium
+import click
+import os
+import fiona
+
+from shapely.geometry import Point, MultiPolygon, Polygon, mapping, shape
+import shapely.wkb as wkblib
+
+wkbfab = osmium.geom.WKBFactory()
+
+
+# https://wiki.openstreetmap.org/wiki/Key:place
+class OSMPlaceHandler(osmium.SimpleHandler):
+    def __init__(self):
+        super(OSMPlaceHandler, self).__init__()
+        self.points = []
+        self.polys = []
+
+    def is_reservation(self, w):
+        return "boundary" in w.tags and w.tags["boundary"] == "aboriginal_lands"
+
+    def is_area(self, w):
+        return (
+            "boundary" in w.tags
+            and w.tags["boundary"] in ["protected_area", "national_park"]
+        ) or (
+            "leisure" in w.tags
+            and w.tags["leisure"] in ["nature_reserve", "park", "dog_park"]
+        )
+
+    def is_place(self, w):
+        return "place" in w.tags and w.tags["place"] in [
+            "city",
+            "town",
+            "village",
+            "hamlet",
+            "suburb",
+            "locality",
+            "island",
+            "islet",
+            "farm",
+            "isolated_dwelling",
+            "neighbourhood",
+        ]
+
+    def node(self, n):
+        if self.is_place(n):
+            self.points.append(
+                (
+                    Point(n.location.lon, n.location.lat),
+                    {
+                        "id": n.id,
+                        "name": n.tags["name"] if "name" in n.tags else None,
+                        "type": n.tags["place"],
+                    },
+                )
+            )
+
+    def area(self, a):
+        if self.is_reservation(a) or self.is_area(a):
+            wkb = wkbfab.create_multipolygon(a)
+            poly = wkblib.loads(wkb, hex=True)
+
+            if self.is_reservation(a):
+                self.polys.append(
+                    (
+                        poly,
+                        {
+                            "id": a.id,
+                            "name": a.tags["name"] if "name" in a.tags else None,
+                            "type": "reservation",
+                            "operator": None,
+                            "protection_title": None,
+                        },
+                    )
+                )
+
+            if self.is_area(a):
+                self.polys.append(
+                    (
+                        poly,
+                        {
+                            "id": a.id,
+                            "name": a.tags["name"] if "name" in a.tags else None,
+                            "type": (
+                                a.tags["boundary"]
+                                if "boundary" in a.tags
+                                else a.tags["leisure"] if "leisure" in a.tags else None
+                            ),
+                            "operator": (
+                                a.tags["operator"] if "operator" in a.tags else None
+                            ),
+                            "protection_title": (
+                                a.tags["protection_title"]
+                                if "protection_title" in a.tags
+                                else None
+                            ),
+                        },
+                    )
+                )
+
+
+@click.command()
+@click.option(
+    "--input-file",
+    type=click.Path(file_okay=True, dir_okay=False),
+    help="The input .osm.pbf file",
+    default="data/sources/extract.osm.pbf",
+)
+@click.option(
+    "--blm-input-file",
+    type=click.Path(file_okay=True, dir_okay=False),
+    help="The input geopackage containing BLM boundaries",
+    default="data/temp/BLM.gpkg",
+)
+@click.option(
+    "--polygon-output-file",
+    type=click.Path(file_okay=True, dir_okay=False),
+    help="The output geopackage for place polygons",
+    default="data/temp/place-polygons.gpkg",
+)
+@click.option(
+    "--point-output-file",
+    type=click.Path(file_okay=True, dir_okay=False),
+    help="The output geopackage for place points",
+    default="data/temp/place-points.gpkg",
+)
+def cli(input_file, blm_input_file, polygon_output_file, point_output_file):
+    # make the path to the output directory if it doesn't exist
+    output_dir = os.path.dirname(polygon_output_file)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    output_dir = os.path.dirname(point_output_file)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    print("Parsing BLM boundaries...")
+    # open the blm geopacakge
+    blm_polys = []
+    with fiona.open(blm_input_file) as src:
+        for feature in src:
+            poly = MultiPolygon(shape(feature["geometry"]))
+            blm_polys.append(
+                (
+                    poly,
+                    {
+                        "id": feature["id"],
+                        "name": "BLM",  # could use the field office name here
+                        "type": "protected_area",
+                        "operator": "Beureau of Land Management",
+                        "protection_title": None,
+                    },
+                )
+            )
+
+    print("Parsing osm data and creating places...")
+    osm_handler = OSMPlaceHandler()
+    osm_handler.apply_file(
+        input_file, locations=True, idx="flex_mem"
+    )  # use idx=dense_mmap_array for large files on linux
+
+    print("Saving places...")
+    schema = {
+        "geometry": "MultiPolygon",
+        "properties": {
+            "id": "int",
+            "name": "str",
+            "type": "str",
+            "operator": "str",
+            "protection_title": "str",
+        },
+    }
+    with fiona.open(
+        polygon_output_file, "w", driver="GPKG", crs="EPSG:4326", schema=schema
+    ) as output:
+        for poly in osm_handler.polys:
+            output.write(
+                {
+                    "geometry": mapping(poly[0]),
+                    "properties": {
+                        "id": poly[1]["id"],
+                        "name": poly[1]["name"],
+                        "type": poly[1]["type"],
+                        "operator": poly[1]["operator"],
+                        "protection_title": poly[1]["protection_title"],
+                    },
+                }
+            )
+
+        for poly in blm_polys:
+            output.write(
+                {
+                    "geometry": mapping(poly[0]),
+                    "properties": {
+                        "id": poly[1]["id"],
+                        "name": poly[1]["name"],
+                        "type": poly[1]["type"],
+                        "operator": poly[1]["operator"],
+                        "protection_title": poly[1]["protection_title"],
+                    },
+                }
+            )
+
+    schema = {
+        "geometry": "Point",
+        "properties": {
+            "id": "int",
+            "name": "str",
+            "type": "str",
+        },
+    }
+    with fiona.open(
+        point_output_file, "w", driver="GPKG", crs="EPSG:4326", schema=schema
+    ) as output:
+        for point in osm_handler.points:
+            output.write(
+                {
+                    "geometry": mapping(point[0]),
+                    "properties": {
+                        "id": point[1]["id"],
+                        "name": point[1]["name"],
+                        "type": point[1]["type"],
+                    },
+                }
+            )
+
+
+if __name__ == "__main__":
+    cli()
