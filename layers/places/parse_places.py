592d06b5ddd0ef33d20a3008c4dccbab78355358
Christian Broms <cb@christianbroms.com>
2024-04-22 21:37:33 -0700

    split place points based on prominence

--- layers/places/parse_places.py
+++ layers/places/parse_places.py
@@ -13,7 +13,9 @@ wkbfab = osmium.geom.WKBFactory()
 class OSMPlaceHandler(osmium.SimpleHandler):
     def __init__(self):
         super(OSMPlaceHandler, self).__init__()
-        self.points = []
+        self.points_prominence_1 = []
+        self.points_prominence_2 = []
+        self.points_prominence_3 = []
         self.polys = []
 
     def is_reservation(self, w):
@@ -43,18 +45,46 @@ class OSMPlaceHandler(osmium.SimpleHandler):
             "neighbourhood",
         ]
 
+    def is_place_prominence1(self, w):
+        return "place" in w.tags and w.tags["place"] in [
+            "city",
+        ]
+
+    def is_place_prominence2(self, w):
+        return "place" in w.tags and w.tags["place"] in [
+            "town",
+            "village",
+        ]
+
+    def is_place_prominence3(self, w):
+        return "place" in w.tags and w.tags["place"] in [
+            "hamlet",
+            "suburb",
+            "locality",
+            "island",
+            "islet",
+            "farm",
+            "isolated_dwelling",
+            "neighbourhood",
+        ]
+
     def node(self, n):
         if self.is_place(n):
-            self.points.append(
-                (
-                    Point(n.location.lon, n.location.lat),
-                    {
-                        "id": n.id,
-                        "name": n.tags["name"] if "name" in n.tags else None,
-                        "type": n.tags["place"],
-                    },
-                )
+            point_data = (
+                Point(n.location.lon, n.location.lat),
+                {
+                    "id": n.id,
+                    "name": n.tags["name"] if "name" in n.tags else None,
+                    "type": n.tags["place"],
+                },
             )
+
+            if self.is_place_prominence1(n):
+                self.points_prominence_1.append(point_data)
+            elif self.is_place_prominence2(n):
+                self.points_prominence_2.append(point_data)
+            elif self.is_place_prominence3(n):
+                self.points_prominence_3.append(point_data)
 
     def area(self, a):
         if self.is_reservation(a) or self.is_area(a):
@@ -209,20 +239,28 @@ def cli(input_file, blm_input_file, polygon_output_file, point_output_file):
             "type": "str",
         },
     }
-    with fiona.open(
-        point_output_file, "w", driver="GPKG", crs="EPSG:4326", schema=schema
-    ) as output:
-        for point in osm_handler.points:
-            output.write(
-                {
-                    "geometry": mapping(point[0]),
-                    "properties": {
-                        "id": point[1]["id"],
-                        "name": point[1]["name"],
-                        "type": point[1]["type"],
-                    },
-                }
-            )
+
+    for point_list, prominence in [
+        (osm_handler.points_prominence_1, 1),
+        (osm_handler.points_prominence_2, 2),
+        (osm_handler.points_prominence_3, 3),
+    ]:
+        print(f"Saving places with prominence {prominence}...")
+        file = point_output_file.replace(".gpkg", f"-prominence-{prominence}.gpkg")
+        with fiona.open(
+            file, "w", driver="GPKG", crs="EPSG:4326", schema=schema
+        ) as output:
+            for point in point_list:
+                output.write(
+                    {
+                        "geometry": mapping(point[0]),
+                        "properties": {
+                            "id": point[1]["id"],
+                            "name": point[1]["name"],
+                            "type": point[1]["type"],
+                        },
+                    }
+                )
 
 
 if __name__ == "__main__":
--- layers/places/tile_places.sh
+++ layers/places/tile_places.sh
@@ -11,10 +11,21 @@ ogr2ogr \
 
 ogr2ogr \
     -f GeoJSONSeq \
-    data/temp/place-points.geojsons \
-    data/temp/place-points.gpkg \
+    data/temp/place-points-prominence-1.geojsons \
+    data/temp/place-points-prominence-1.gpkg \
     -progress
 
+ogr2ogr \
+    -f GeoJSONSeq \
+    data/temp/place-points-prominence-2.geojsons \
+    data/temp/place-points-prominence-2.gpkg \
+    -progress
+
+ogr2ogr \
+    -f GeoJSONSeq \
+    data/temp/place-points-prominence-3.geojsons \
+    data/temp/place-points-prominence-3.gpkg \
+    -progress
 
 echo -e "\nTiling dataset...\n"
 
@@ -32,11 +43,39 @@ tippecanoe \
 tippecanoe \
     -Z1 \
     -z16 \
-    -o data/temp/place-points.pmtiles \
-    -l places \
-    --drop-densest-as-needed \
+    -r1 \
+    --no-feature-limit \
+    --no-tile-size-limit \
+    -o data/temp/place-points-prominence-1.pmtiles \
+    -l places_p1 \
     --read-parallel \
-    data/temp/place-points.geojsons \
+    data/temp/place-points-prominence-1.geojsons \
+    -P \
+    --force
+
+tippecanoe \
+    -Z1 \
+    -z16 \
+    -r1 \
+    --no-feature-limit \
+    --no-tile-size-limit \
+    -o data/temp/place-points-prominence-2.pmtiles \
+    -l places_p2 \
+    --read-parallel \
+    data/temp/place-points-prominence-2.geojsons \
+    -P \
+    --force
+
+tippecanoe \
+    -Z11 \
+    -z16 \
+    -r1 \
+    --no-feature-limit \
+    --no-tile-size-limit \
+    -o data/temp/place-points-prominence-3.pmtiles \
+    -l places_p3 \
+    --read-parallel \
+    data/temp/place-points-prominence-3.geojsons \
     -P \
     --force
 
@@ -45,6 +84,9 @@ echo -e "\nCombining place points and polygons...\n"
 tile-join \
     -o data/output/places.pmtiles \
     data/temp/place-polygons.pmtiles \
-    data/temp/place-points.pmtiles --force
+    data/temp/place-points-prominence-1.pmtiles \
+    data/temp/place-points-prominence-2.pmtiles \
+    data/temp/place-points-prominence-3.pmtiles \
+    --force
 
 echo -e "\n\nDone, created: \ndata/output/places.pmtiles\n"
